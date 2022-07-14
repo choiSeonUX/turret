01de33e2738a43db640f84138e39cfd4de30e0b9
choiSeonUX <103400288+choiSeonUX@users.noreply.github.com>
2022-07-15 08:28:42 +0900

    Add files via upload

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody;
+    public float speed = 3f;
+    public float degreePerSecond = 50f;
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed; // ¼Óµµ 
+
+        Destroy(gameObject, 3f);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerControl playcontrol = other.GetComponent<PlayerControl>();
+
+            if (playcontrol != null)
+            {
+                playcontrol.Die();
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Turret/Cube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour        
+{
+    public GameObject bulletPrefab;
+    public float makeBulletMin = 0.5f;
+    public float makeBulletMax = 3f;
+    public float speed = 5f;
+
+    private Transform target;
+    private float makeBullet;
+    private float elaspedtime;
+
+    private void Start()
+    {
+        elaspedtime = 0f;
+        makeBullet = Random.Range(makeBulletMin, makeBulletMax);
+        target = FindObjectOfType<PlayerControl>().transform;
+
+    }
+
+    void Update()
+    {
+        elaspedtime += Time.deltaTime;
+
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, 0f);
+
+        if (elaspedtime >= makeBullet)
+        {
+            elaspedtime = 0;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+            makeBullet = Random.Range(makeBulletMin, makeBulletMax);
+        }
+
+        
+        Vector3 distanceVector = target.position - transform.position;
+
+        Debug.DrawRay(transform.position, distanceVector, Color.blue); 
+
+
+
+    }
+}
